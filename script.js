6f8bf733a9bfbad18385fc03e02249edf36ae93a
Blessing Mainga <Bida25-129@thuto.bac.ac.bw>
2026-04-19 03:32:47 +0200

    Initial commit of Blessing Fitness Club website

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("JavaScript is connected!");
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            toggleButton.textContent = 'light Mode';
+        } else {
+            toggleButton.textContent = 'Dark Mode';
+        }
+    });
